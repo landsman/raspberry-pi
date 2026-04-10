6d7f0fa7270621881fb088fd630d71858a1ec9ec
Michal Landsman <landsman@studioart.cz>
2026-04-10 16:17:38 +0200

    ui tweaks

--- welcome-page/src/routes/__root.tsx
+++ welcome-page/src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { createRootRoute, Outlet, useNavigate } from '@tanstack/react-router'
 import { Header } from '../app/components/header'
 import { Footer } from '../app/components/footer'
 import { ConfigModal } from '../app/config/components/config-modal'
+import { SearchProvider } from '../app/search-context'
 import { useHotkey } from '../app/hooks/use-hotkey'
 import { ROUTES } from '../app/routes'
 
@@ -20,19 +21,21 @@ function RootLayout() {
   )
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="w-full flex-1 flex flex-col">
-        <Header onSettingsClick={() => setSettingsOpen(true)} />
+    <SearchProvider>
+      <div className="min-h-screen flex flex-col">
+        <div className="w-full flex-1 flex flex-col">
+          <Header onSettingsClick={() => setSettingsOpen(true)} />
 
-        <main className="px-6 md:px-10 xl:px-16 pt-8 pb-12 flex-1">
-          <Outlet />
-        </main>
+          <main className="px-6 md:px-10 xl:px-16 pt-8 pb-12 flex-1">
+            <Outlet />
+          </main>
 
-        <Footer onSettingsClick={() => setSettingsOpen(true)} />
+          <Footer onSettingsClick={() => setSettingsOpen(true)} />
+        </div>
+
+        {settingsOpen && <ConfigModal onClose={() => setSettingsOpen(false)} />}
       </div>
-
-      {settingsOpen && <ConfigModal onClose={() => setSettingsOpen(false)} />}
-    </div>
+    </SearchProvider>
   )
 }
 
--- welcome-page/src/routes/status.tsx
+++ welcome-page/src/routes/status.tsx
@@ -1,21 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
 import { StatusPage } from '../features/status-page/status-page'
-import { SearchBar } from '../features/status-page/components/search-bar'
+import { useSearch } from '../app/search-context'
 
 export const Route = createFileRoute('/status')({
   component: StatusRoute,
 })
 
 function StatusRoute() {
-  const [query, setQuery] = useState('')
-
-  return (
-    <>
-      <div className="mb-6 md:hidden">
-        <SearchBar query={query} onChange={setQuery} />
-      </div>
-      <StatusPage query={query} onQueryChange={setQuery} />
-    </>
-  )
+  const { query, setQuery } = useSearch()
+  return <StatusPage query={query} onQueryChange={setQuery} />
 }
