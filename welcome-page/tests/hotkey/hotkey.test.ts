95dc0936c4297798413db3f5a92738f7cf45d19d
Michal Landsman <landsman@studioart.cz>
2026-04-10 16:36:33 +0200

    format

--- welcome-page/tests/hotkey/hotkey.test.ts
+++ welcome-page/tests/hotkey/hotkey.test.ts
@@ -3,7 +3,9 @@ import { HOME_CATEGORIES } from '../../src/features/home/data/services'
 
 describe('home hotkeys', () => {
   it('hotkey have to be unique', async () => {
-    const shortcuts = HOME_CATEGORIES.flatMap(i => i.services.map(s => s.shortcut)).filter(s => s !== undefined)
+    const shortcuts = HOME_CATEGORIES.flatMap(i => i.services.map(s => s.shortcut)).filter(
+      s => s !== undefined
+    )
     expect(new Set(shortcuts).size).toBe(shortcuts.length)
   })
 })
